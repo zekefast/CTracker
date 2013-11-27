6f5dbd02bd7923742a78ece78a787fd75bf4ee4f
Zeke Fast <zekefast@gmail.com>
2013-11-27 19:08:00 +0200

    Upgrade to Ruby 2.0.0 and Ruby on Rails 4.0.1.
    All existing files and configuration were fixed accordingly.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CurrencyTracker::Application.config.secret_token = '6725af6ed78f3b1f346be407ac50359be53dc023b31fb1d673cac36c6bc1681719c93455b1dcdcfe6ebcd37b3495781b42507994dcbef009f89efcd95249b08b'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurrencyTracker::Application.config.secret_key_base = '29621ed3eebcd66900d89a7c259a3f3ff9aa5fc8555bb5907d8710b2272f456416689e853dd8683aa9a4169643b873e5c9c48e5b35dd6fb27237a4db64ad49ca'
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-
-DataUpdater.update
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
