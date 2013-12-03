52c92a70e541778412af64b582be8182d305728b
Zeke Fast <zekefast@gmail.com>
2013-12-03 18:41:45 +0200

    Upgrade db/schema.rb with one generated by Rails 4.
    In particularly it replace hash operator from => to :.

--- db/schema.rb
+++ db/schema.rb
@@ -9,19 +9,19 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110425153204) do
+ActiveRecord::Schema.define(version: 20110425153204) do
 
-  create_table "countries", :id => false, :force => true do |t|
+  create_table "countries", id: false, force: true do |t|
     t.string   "name"
     t.string   "code"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.boolean  "visited",    :default => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean  "visited",    default: false
   end
 
-  create_table "currencies", :id => false, :force => true do |t|
+  create_table "currencies", id: false, force: true do |t|
     t.string   "name"
     t.string   "code"
     t.datetime "created_at", :null => false
