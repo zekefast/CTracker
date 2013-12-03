934110244cc7ec944ad4240cd43640067276398f
Zeke Fast <zekefast@gmail.com>
2013-12-03 18:37:51 +0200

    Fix broken tests caused by upgrade to Ruby on Rails 4.
    Change expected class of exception in helper method.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,7 +15,7 @@ class ActiveSupport::TestCase
   def self.should_not_respond_to_actions(actions = {})
     actions.each do |name, method|
       test "should not respond to #{method.to_s.upcase} :#{name}" do
-        assert_raise ActionController::RoutingError do
+        assert_raise ActionController::UrlGenerationError do
           send(method, name)
         end
       end
