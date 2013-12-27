511f8033fbc18a9495996daf2d9a3b529aa923fa
Zeke Fast <zekefast@gmail.com>
2013-12-27 18:04:05 +0200

    Update db/schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20110425153204) do
+ActiveRecord::Schema.define(version: 20131205133144) do
 
-  create_table "countries", id: false, force: true do |t|
+  create_table "collections", force: true do |t|
+    t.integer  "countries_currency_id", null: false
+    t.integer  "user_id",               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "collections", ["countries_currency_id", "user_id"], name: "index_collections_on_countries_currency_id_and_user_id", unique: true
+  add_index "collections", ["countries_currency_id"], name: "index_collections_on_countries_currency_id"
+  add_index "collections", ["user_id"], name: "index_collections_on_user_id"
+
+  create_table "countries", force: true do |t|
     t.string   "name"
     t.string   "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean  "visited",    default: false
   end
 
-  create_table "currencies", id: false, force: true do |t|
+  add_index "countries", ["code"], name: "index_countries_on_code", unique: true
+
+  create_table "countries_currencies", force: true do |t|
+    t.integer  "country_id",  null: false
+    t.integer  "currency_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "countries_currencies", ["country_id", "currency_id"], name: "index_countries_currencies_on_country_id_and_currency_id", unique: true
+  add_index "countries_currencies", ["country_id"], name: "index_countries_currencies_on_country_id"
+  add_index "countries_currencies", ["currency_id"], name: "index_countries_currencies_on_currency_id"
+
+  create_table "currencies", force: true do |t|
     t.string   "name"
     t.string   "code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "country_id"
   end
+
+  add_index "currencies", ["code"], name: "index_currencies_on_code", unique: true
+
+  create_table "users", force: true do |t|
+    t.string   "email",               null: false
+    t.string   "encrypted_password",  null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
