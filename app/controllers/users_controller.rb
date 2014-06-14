f22f225b863a9cebc1d94577d2c1b443c5d516a1
Zeke Fast <zekefast@gmail.com>
2014-06-15 02:12:52 +0300

    Format code.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,7 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-    def user_params
-      params.require(:user).permit(countries_currencies: [])
-    end
+  private def user_params
+    params.require(:user).permit(countries_currencies: [])
+  end
 end
